a892279258305a20bb172ff1adc1826b811f60bd
Saul Nunez <saul.nunez99@gmail.com>
2025-11-23 15:06:11 -0700

    Application details

--- src/app/application-details/application-details.ts
+++ src/app/application-details/application-details.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { Application } from '../application';
+import { ApplicationService } from '../application-service';
 
 @Component({
   selector: 'app-application-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './application-details.html',
   styleUrl: './application-details.css',
 })
 export class ApplicationDetails {
+  private applicationService = inject(ApplicationService);
+  private route = inject(ActivatedRoute);
 
+  protected application = signal<Application | null>(null);
+  protected loading = signal(true);
+
+  constructor() {
+    const { applicationId } = this.route.snapshot.params;
+    this.applicationService.getApplicationById(applicationId).subscribe((value) => {
+      this.application.set(value);
+      this.loading.set(false);
+    });
+  }
 }
--- src/app/application-service.ts
+++ src/app/application-service.ts
@@ -14,6 +14,12 @@ export class ApplicationService {
       context: withCache()
     });
   }
+  getApplicationById(applicationId: string){
+    return this.httpClient.get<Application>(`/api/developer/application/${applicationId}`, {
+      context: withCache()
+    });
+  }
+
   getWindowsReleaseById(applicationId: string, releaseId: string) {
 
   }
